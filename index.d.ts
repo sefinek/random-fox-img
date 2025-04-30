649488d29b8963f0fe2f855af70deff8a4ecec6b
Sefinek <contact@sefinek.net>
2025-04-30 12:29:04 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,18 @@
-declare module 'random-fox-img' {
-    interface Info {
-        category: string;
-        endpoint: string;
-    }
-
-    interface GetRandomFox {
-        success: boolean;
-        status: number;
-        info: Info;
-        message: string;
-    }
-
-    /**
-     * Retrieves a random fox object from the specified API.
-     *
-     * @async
-     * @returns {Promise<GetRandomFox>} A promise that resolves with a random fox object on success or rejects with an error on failure.
-     * @throws {Error} If there's an error in making the request, parsing JSON data, or if the API responds with a non-200 status code.
-     */
-    export function getRandomFox(): Promise<GetRandomFox>;
+export interface Info {
+    category: string;
+    endpoint: string;
 }
+
+export interface GetRandomFox {
+    success: boolean;
+    status: number;
+    info: Info;
+    message: string;
+}
+
+/**
+ * Fetches a random fox object from the API.
+ * @returns A promise that resolves to a random fox object on success.
+ * @throws If the request fails, the response is invalid, or JSON parsing fails.
+ */
+export function getRandomFox(): Promise<GetRandomFox>;
